e1d88d90d541927767d21498ad54b71d765df34a
Filipe Bello <filipeg.bello@gmail.com>
2024-08-28 23:08:46 -0300

    commit 5

--- 02-api-rest-nodejs/src/database.ts
+++ 02-api-rest-nodejs/src/database.ts
@@ -1,14 +1,14 @@
 import 'dotenv/config'
 import { knex as setupKnex, Knex } from 'knex'
+import { env } from '../env'
 
-if (!process.env.DATABASE_URL) {
-  throw new Error('No DATABASE_URL environment variable found.')
-}
+
+
 
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: process.env.DATABASE_URL,
+    filename: env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
